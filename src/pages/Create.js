4d331b7bea93a4ff8a5f5ed48401cb89bece7303
Prahlad Inala <prahlad2018@gmail.com>
2021-09-15 15:17:51 +0530

    Buttons

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 
 export default function Create() {
   return (
@@ -12,6 +14,25 @@ export default function Create() {
       >
         Create a new notes
       </Typography>
+
+      <Button
+        onClick={() => console.log('You Clicked me')}
+        type="submit"
+        color="secondary"
+        variant="contained" >
+        Submit
+      </Button>
+      <Button type="submit"> Submit </Button>
+      <Button type="submit" color="secondary" variant="outlined"> Submit </Button>
+      <Button type="submit" color="primary" variant="contained"> Submit </Button>
+      <Button type="submit" color="primary" variant="contained" disableElevation> Submit </Button>
+
+      <ButtonGroup color="secondary" variant="contained">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup>
+
     </div>
   )
 }
