4074dcc7d9db8c31a8a598184821d215da1fef64
Prahlad Inala <prahlad2018@gmail.com>
2021-09-15 15:07:09 +0530

    Typography

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -4,20 +4,13 @@ import Typography from '@material-ui/core/Typography'
 export default function Create() {
   return (
     <div>
-      Create page
-      {/* <Typography
-        variant="h1"
-        color="primary"
-        align="center"
-      >
-        Create a new Note
-      </Typography> */}
-
       <Typography
-        noWrap
-        color="secondary"
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
       >
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        Create a new notes
       </Typography>
     </div>
   )
