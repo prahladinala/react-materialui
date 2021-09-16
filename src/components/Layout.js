9c03ea4f317b9be91eb2d393a028476757789468
Prahlad Inala <prahlad2018@gmail.com>
2021-09-16 21:03:41 +0530

    List ListItem ListItemText ListItemIcon Dynamic_by_using_variable Dyamic ClassName

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,6 +6,8 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import { AddCircleOutlineOutlined, SubjectOutlined } from '@material-ui/icons';
+import { useHistory, useLocation } from 'react-router';
 
 const drawerWidth = 240
 
@@ -22,11 +24,29 @@ const useStyles = makeStyles({
     },
     root: {
         display: 'flex'
+    },
+    active: {
+        background: '#f4f4f4'
     }
 })
 
 export default function Layout({ children }) {
     const classes = useStyles()
+    const history = useHistory()
+    const location = useLocation()
+    const menuItems = [
+        {
+            text: 'My Notes',
+            icon: <SubjectOutlined color="secondary" />,
+            path: '/'
+        },
+        {
+            text: 'Create Note',
+            icon: <AddCircleOutlineOutlined color="secondary" />,
+            path: '/create'
+        },
+    ];
+
     return (
         <div className={classes.root}>
             {/* APP BAR */}
@@ -43,21 +63,24 @@ export default function Layout({ children }) {
                     </Typography>
                 </div>
 
+                {/* LIST / LINKS */}
                 <List>
-                    <ListItem>
-                        <ListItemText primary="hello" />
-                    </ListItem>
-                    <ListItem>
-                        <ListItemText primary="hello" />
-                    </ListItem>
-                    <ListItem>
-                        <ListItemText primary="hello" />
-                    </ListItem>
+                    {menuItems.map((item) => (
+                        <ListItem
+                            button
+                            key={item.text}
+                            onClick={() => history.push(item.path)}
+                            className={location.pathname == item.path ? classes.active : null}
+                        >
+                            <ListItemIcon>{item.icon}</ListItemIcon>
+                            <ListItemText primary={item.text} />
+                        </ListItem>
+                    ))}
                 </List>
             </Drawer>
             <div className={classes.page}>
                 {children}
             </div>
-        </div>
+        </div >
     )
 }
