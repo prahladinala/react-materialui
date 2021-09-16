851904dad68a0565e566d7ffc2236d6179e4a70a
Prahlad Inala <prahlad2018@gmail.com>
2021-09-16 21:11:35 +0530

    useStyles makeStyles > passing as a function (Layout.js) and by passing arguments(NoteCard.js)

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,22 +11,29 @@ import { useHistory, useLocation } from 'react-router';
 
 const drawerWidth = 240
 
-const useStyles = makeStyles({
-    page: {
-        background: '#f9f9f9',
-        width: '100%'
-    },
-    drawer: {
-        width: drawerWidth,
-    },
-    drawerPaper: {
-        width: drawerWidth,
-    },
-    root: {
-        display: 'flex'
-    },
-    active: {
-        background: '#f4f4f4'
+const useStyles = makeStyles((theme) => {
+    return {
+        page: {
+            background: '#f9f9f9',
+            width: '100%',
+            padding: theme.spacing(3)
+            // theme.spacing(3) > BASE SPACING ie 8px * 3
+        },
+        drawer: {
+            width: drawerWidth,
+        },
+        drawerPaper: {
+            width: drawerWidth,
+        },
+        root: {
+            display: 'flex'
+        },
+        active: {
+            background: '#f4f4f4'
+        },
+        title: {
+            padding: theme.spacing(2)
+        }
     }
 })
 
@@ -58,7 +65,7 @@ export default function Layout({ children }) {
                 classes={{ paper: classes.drawerPaper }}
             >
                 <div>
-                    <Typography variant="h5">
+                    <Typography variant="h5" className={classes.title}>
                         Revocta Notes
                     </Typography>
                 </div>
--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -4,12 +4,24 @@ import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import IconButton from '@material-ui/core/IconButton';
 import { DeleteOutline } from '@material-ui/icons';
-import { Typography } from '@material-ui/core';
+import { makeStyles, Typography } from '@material-ui/core';
 
+const useStyles = makeStyles({
+    test: {
+        border: (note) => {
+            if (note.category == 'work') {
+                return '1px solid red'
+            }
+        }
+    }
+})
 export default function NoteCard({ note, handleDelete }) {
+
+    const classes = useStyles(note)
+
     return (
         <div>
-            <Card elevation={1}>
+            <Card elevation={1} className={classes.test}>
                 <CardHeader
                     action={
                         <IconButton onClick={() => handleDelete(note.id)}>
