e8f18c88c5b321e30dd3d877d8a272e864db91be
Prahlad Inala <prahlad2018@gmail.com>
2021-09-16 13:39:49 +0530

    Basic Grid system Example

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
 
 export default function Notes() {
 
@@ -15,6 +17,20 @@ export default function Notes() {
 
   return (
     <div>
+      <Grid container>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>1</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>2</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>3</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>4</Paper>
+        </Grid>
+      </Grid>
       {notes.map(note => (
         <p key={note.id}>{note.title}</p>
       ))}
