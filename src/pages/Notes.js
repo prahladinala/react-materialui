ff3c2a4b0132bd9d33cf42896b58805e0960f2a7
Prahlad Inala <prahlad2018@gmail.com>
2021-09-16 23:03:24 +0530

    react-masonry-css

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -3,6 +3,8 @@ import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import { Container } from '@material-ui/core'
 import NoteCard from '../components/NoteCard'
+import Masonry from 'react-masonry-css'
+
 export default function Notes() {
 
   const [notes, setNotes] = useState([])
@@ -25,16 +27,27 @@ export default function Notes() {
     setNotes(newNotes)
   }
 
+  const breakpoints = {
+    default: 3,
+    1100: 2,
+    700: 1
+  }
+
   return (
     <Container>
-      <Grid container spacing={3}>
+      <Masonry
+        breakpointCols={breakpoints}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column">
         {notes.map(note => (
-          <Grid item key={note.id} xs={12} sm={6} md={3} lg={4}>
+          <div key={note.id}>
             {/* <Paper>{note.title}</Paper> */}
             <NoteCard note={note} handleDelete={handleDelete} />
-          </Grid>
+          </div>
         ))}
-      </Grid>
+      </Masonry>
     </Container>
   )
 }
+// REACT MASONRY CSS
+// https://www.npmjs.com/package/react-masonry-css
