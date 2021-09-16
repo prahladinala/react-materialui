e87d13a38a6c3d29d8912c41aa0f84c1a7c446d8
Prahlad Inala <prahlad2018@gmail.com>
2021-09-16 13:31:33 +0530

    JSON Server POST Request in Create.js page

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -12,6 +12,7 @@ import Radio from '@material-ui/core/Radio'
 import RadioGroup from '@material-ui/core/RadioGroup'
 import FormControl from '@material-ui/core/FormControl'
 import FormLabel from '@material-ui/core/FormLabel'
+import { useHistory } from 'react-router';
 
 const useStyles = makeStyles({
   field: {
@@ -22,6 +23,8 @@ const useStyles = makeStyles({
 })
 export default function Create() {
   const classes = useStyles()
+
+  const history = useHistory()
 
   const [title, setTitle] = useState('')
   const [details, setDetails] = useState('')
@@ -43,7 +46,12 @@ export default function Create() {
       setDetailsError(true)
     }
     if (title && details) {
-      console.log(title, details, category)
+      // console.log(title, details, category)
+      fetch('http://localhost:8000/notes', {
+        method: 'POST',
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({ title, details, category })
+      }).then(() => history.push('/'))
     }
   }
   return (
